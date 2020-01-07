af19f6345442de864763ab99562a13e2d67af324
Charlie Kindel <charlie@kindel.com>
2020-01-07 07:04:53 -0800

    wix stuff

--- src/WinPrint.Core/Models/Uuid.cs
+++ src/WinPrint.Core/Models/Uuid.cs
@@ -8,6 +8,9 @@ namespace WinPrint.Core {
     //
     // Available range: 0002A5xx-0000-0000-C000-000000000046
     //
+    // {0002A500-0000-0000-C000-000000000046} - Wix Installer WinPrint UpgradeCode (product family code), Default Sheet ID 
+    // {0002A501-0000-0000-C000-000000000046} - Wix Installer Product Code, Default 1SheetUp ID 
+    //
     public static class Uuid {
         public static readonly Guid DefaultSheet = Guid.Parse("{0002A500-0000-0000-C000-000000000046}");
         public static readonly Guid DefaultSheet1Up = Guid.Parse("{0002A501-0000-0000-C000-000000000046}");
