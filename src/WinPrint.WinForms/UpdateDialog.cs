2261729b0543119667b94767b6c7e2cc3eab662a
Charlie Kindel <tig@users.noreply.github.com>
2020-04-05 10:19:28 -0600

    Fixed update dialog link to release page.

--- src/WinPrint.WinForms/UpdateDialog.cs
+++ src/WinPrint.WinForms/UpdateDialog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using Serilog;
 using WinPrint.Core.Services;
 
 namespace WinPrint.WinForms {
@@ -16,8 +17,27 @@ namespace WinPrint.WinForms {
             ServiceLocator.Current.UpdateService.StartUpgradeAsync().ConfigureAwait(false);
         }
 
-        private void linkReleasePage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
-            Process.Start((string)linkReleasePage.Links[0].LinkData);
+        private void linkReleasePage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs args) {
+            Log.Debug($"Browsing to release page: {(string)linkReleasePage.Links[0].LinkData}");
+
+            ServiceLocator.Current.TelemetryService.TrackEvent("Release Page Click");
+
+            Process proc = null;
+            try {
+                var psi = new ProcessStartInfo();
+                psi.UseShellExecute = true;   // This is important
+                psi.FileName = (string)linkReleasePage.Links[0].LinkData;
+                proc = Process.Start(psi);
+            }
+            catch (Exception e) {
+                // TODO: Better error message (output of stderr?)
+                ServiceLocator.Current.TelemetryService.TrackException(e, false);
+
+                Log.Error(e, $"Couldn't browse to {(string)linkReleasePage.Links[0].LinkData}.");
+            }
+            finally {
+                proc?.Dispose();
+            }
         }
     }
 }
