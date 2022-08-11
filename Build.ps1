7f3205b867a328f6ba2475e8ac694fd01e32af7a
Tig Kindel <tig@users.noreply.github.com>
2022-08-10 18:56:41 -0600

    updateed build script

--- Build.ps1
+++ Build.ps1
@@ -1,10 +1,12 @@
-echo "Setting up VS2022 Shell"
-#cmd.exe "/K" '"C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\Tools\VsDevCmd.bat" && pwsh -noexit' 
+echo "If build fails, do this:"
+echo "cmd.exe ""/K"" '""C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\Tools\VsDevCmd.bat"" && pwsh -noexit'"
 
 echo "Cleaning Deploy folder"
 rm -Recurse -Path setup/Deploy -Include *.* 
 
 echo "Building"
+$env:winprint_telemetryId="put Kindel Systems key here"
+texttransform .\src\WinPrint.Core\Services\TelemetryService.tt
 msbuild /p:Configuration=Debug /p:Platform=x64 src/WinPrint.sln 
 
 echo "Testing Out-winprint"
