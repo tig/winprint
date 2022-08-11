b6c480d06d91fcf68cd87e589b4ece4504810af2
Tig Kindel <tig@users.noreply.github.com>
2022-08-10 18:04:44 -0600

    build

--- Build.ps1
+++ Build.ps1
@@ -1,8 +1,5 @@
 echo "Setting up VS2022 Shell"
-#Import-Module -Verbose "C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-#Enter-VsDevShell dff05ec8 -DevCmdDebugLevel Basic -SkipAutomaticLocation -DevCmdArguments """-arch=x64 -host_arch=x64"""
-
- $Env:VCTargetsPath="C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Microsoft\VC\v170\"
+#cmd.exe "/K" '"C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\Tools\VsDevCmd.bat" && pwsh -noexit' 
 
 echo "Cleaning Deploy folder"
 rm -Recurse -Path setup/Deploy -Include *.* 
